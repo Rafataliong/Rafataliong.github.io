331754ae6a766f960d65cbc5a310f2070489bb95
Rafataliong <50372261+Rafataliong@users.noreply.github.com>
2019-06-10 17:48:36 -0300

    Add files via upload

---
+++ Game1/offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1560199648,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/starshipcontroller-sheet0.png",
+		"images/setacima-sheet0.png",
+		"images/setacima-sheet1.png",
+		"images/bulletcontroller-sheet0.png",
+		"images/shootbutton-sheet0.png",
+		"images/shootbutton-sheet1.png",
+		"images/enemycontroller-sheet0.png",
+		"images/background.png",
+		"images/barravida-sheet0.png",
+		"images/contornobarravida-sheet0.png",
+		"images/powerup-sheet0.png",
+		"images/sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
